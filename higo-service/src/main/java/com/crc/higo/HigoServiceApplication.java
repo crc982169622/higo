6a60cae7ed8d7bf9bccd8466ac03492d1089a212
crc982169622 <982169622@qq.com>
2019-08-11 11:42:57 +0800

    springboot框架搭建

---
+++ higo-service/src/main/java/com/crc/higo/HigoServiceApplication.java
@@ -0,0 +1,13 @@
+package com.crc.higo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HigoServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HigoServiceApplication.class, args);
+    }
+
+}
